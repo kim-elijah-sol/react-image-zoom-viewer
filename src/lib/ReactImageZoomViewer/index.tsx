e0a3f51243e7403eeb123967a001e7a934a38f5b
kim-elijah-sol <kimelijahsol@gmail.com>
2023-11-08 23:40:19 +0900

    feat: 컴포넌트 props 설계, children 검사식 추가

--- src/lib/ReactImageZoomViewer/index.tsx
+++ src/lib/ReactImageZoomViewer/index.tsx
@@ -1,5 +1,25 @@
-function ReactImageZoomViewer() {
-  return <></>
+import { Children } from 'react'
+
+type ImgProps = JSX.IntrinsicElements['img']
+
+type ImgElement = React.ReactElement<ImgProps>
+
+interface Props {
+  children: ImgElement
+}
+
+function ReactImageZoomViewer({ children }: Props) {
+  const img = Children.only(children)
+
+  if (img.type !== 'img') {
+    throw new Error('The only child must be an img element')
+  }
+
+  if (!img.props.src) {
+    throw new Error('The img element must have a src attribute')
+  }
+
+  return <>{children}</>
 }
 
 export default ReactImageZoomViewer
